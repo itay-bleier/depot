90a8f268c0af37c1a9e7f90e2ff59773cf6c329d
Itay <itay.bleier@naturalint.com>
2014-01-05 11:34:56 +0200

    before laptop switch

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+     assert true
+  end
+
+  test "product_attrs_non_empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+  end
 end
